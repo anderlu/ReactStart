696b46d3b3c4d7edcc92c34228489669e536748b
anderlu <anderlublaze@gmail.com>
2015-07-24 18:27:39 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
+var reload = browserSync.reload;
+var react = require('gulp-react');
 
 gulp.task('webserver', function() {
     gulp.src('src')
@@ -19,6 +20,12 @@ gulp.task('webserver', function() {
         }));
 });
 
+gulp.task('transformJsx', function() {
+    gulp.src('src/demo1/*.jsx')
+        .pipe(react())
+        .pipe(gulp.dest('src/demo1/'));
+});
+
 gulp.task('browser-sync', function () {
     var files = [
         'src/**/*.html',
@@ -28,10 +35,8 @@ gulp.task('browser-sync', function () {
 
     // Serve files from the root of this project
     browserSync.init(files,{
-        open: false,
-        ui: {
-            port: 8964
-        },
+        open: true,
+        port: 8964,
         server: {
             baseDir: "./src"
         }
--- src/demo1/app.jsx
+++ src/demo1/app.jsx
@@ -3,14 +3,6 @@
  * Date: 15/7/9
  * Time: 下午3:42
  */
-//var div = React.createElement('div', null, "Hello React");
-//
-//// 使用 JSX
-//var div = <div>Hello React</div> ;
-//
-//React.render(div, document.body);
-
-
 var HelloMessage = React.createClass({
     render: function () {
         return <div>Hello {this.props.name}</div> ;
